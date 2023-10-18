5eaecef76d0dfcf9437fb58c68ab650245978203
吕超凡 <21373104@buaa.edu.cn>
2023-10-19 00:13:55 +0800

    一事无成

--- genius/src/test/java/com/example/genius/Generator.java
+++ genius/src/test/java/com/example/genius/Generator.java
@@ -51,6 +51,7 @@ public class Generator {
                 .strategyConfig(builder -> {
                     builder.addInclude(tables) // 设置需要生成的表名
                             .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+
                 })
 
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
