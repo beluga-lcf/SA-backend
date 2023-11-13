c4713e98626ebf7fe6679d8c55736eb3b50b4146
wenjiaran <101538084+WenjiaRan@users.noreply.github.com>
2023-11-13 19:31:01 +0800

    add sql commands

--- genius/src/main/resources/customer-pg.sql
+++ genius/src/main/resources/customer-pg.sql
@@ -20,3 +20,64 @@ create table customer.verify_code (
     create_time timestamp default current_timestamp
 );
 
+drop table if exists customer.comment;
+create table customer.comment
+(
+    id                serial
+        primary key,
+    content           text    not null,
+    user_id           integer not null
+        references customer."user",
+    work_id           text    not null,
+    time              timestamp default CURRENT_TIMESTAMP,
+    parent_comment_id integer
+        references customer.comment,
+    like_count        integer   default 0
+);
+
+drop table if exists customer.comment_likes;
+create table customer.comment_likes
+(
+    comment_id integer not null
+        references customer.comment,
+    user_id    integer not null
+        references customer."user",
+    primary key (comment_id, user_id)
+);
+
+create trigger like_count_update
+    after insert or delete
+    on customer.comment_likes
+    for each row
+execute procedure public.update_like_count();
+
+create function update_like_count() returns trigger
+    language plpgsql
+as
+$$
+BEGIN
+    IF (TG_OP = 'INSERT') THEN
+        -- 点赞时增加点赞数
+        UPDATE customer.comment SET like_count = like_count + 1
+        WHERE id = NEW.comment_id;
+        RETURN NEW;
+    ELSIF (TG_OP = 'DELETE') THEN
+        -- 取消点赞时减少点赞数
+        UPDATE customer.comment SET like_count = like_count - 1
+        WHERE id = OLD.comment_id;
+        RETURN OLD;
+    END IF;
+    RETURN NULL;
+END;
+$$;
+
+
+drop table if exists customer.comment_mentions;
+create table customer.comment_mentions
+(
+    comment_id        integer not null
+        references customer.comment,
+    mentioned_user_id integer not null
+        references customer."user",
+    primary key (comment_id, mentioned_user_id)
+);
